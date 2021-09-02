ab459561bf6218a412115ccb99bf053d35d341a0
Lee GyeongJun <cockyb@nate.com>
2021-09-02 11:09:22 +0900

    Feat: input autofocus #2

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -5,16 +5,20 @@ class SearchInput {
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.autofocus = true
 
-    $searchInput.className = "SearchInput";
-    $target.appendChild($searchInput);
+    this.$searchInput.className = "SearchInput";
 
-    $searchInput.addEventListener("keyup", e => {
+    this.$searchInput.addEventListener("keyup", e => {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
       }
     });
+    this.$searchInput.addEventListener("click", (e) => {
+      e.target.value = ''
+    })
 
+    $target.appendChild($searchInput);
     console.log("SearchInput created.", this);
   }
   render() { }
